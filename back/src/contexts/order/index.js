6fe70501c6602499c0c65d195acb818624bd980f
lina-bkd <96200016+lina-bkd@users.noreply.github.com>
2024-04-04 16:21:01 +0200

    add request to modify orders

--- back/src/contexts/order/index.js
+++ back/src/contexts/order/index.js
@@ -110,19 +110,80 @@ router.delete("/", async (req, res) => {
     res.status(200).json(orders);
 });
 
-//modify the status of an order
+
+//modify an order if the status is 'in progress' else return an error
 router.put("/:id", async (req, res) => {
     const { id } = req.params;
-    const { status } = req.body;
-    const order = await client.order.update({
+    const { articles, status } = req.body;
+    let totalPrice = 0.0;
+    let listOrder = [];
+    let fetchPromises = [];
+    let count = 0;
+    let price = 0.0;
+
+    const order = await client.order.findUnique({
         where: {
-            id: id
-        },
-        data: {
-            status: status
+            id: id 
         }
     });
-    res.status(200).json(order);
+
+    if (articles === undefined) {
+        //on modifie juste le status
+        const order = await client.order.update({
+            where: {
+                id: id
+            },
+            data: {
+                status: status
+            }
+        });
+        res.status(200).json(order);
+    }else{
+        if ((order.status).toLowerCase() === 'in progress') {
+            for (let i = 0; i < articles.length; i++) {
+                const article = articles[i];
+                const articleId = article.id;
+                const size = article.size;
+                const response = fetch(`${urlStock}/stocks/${articleId}`).then(response => response.json()).then(data => {
+                    for (stock of data.stocks) {
+                        if (stock.size === size) {
+                            count = stock.count;
+                            price = stock.price;
+                        }
+                    }
+                    if (count > 0) {
+                        totalPrice += price;
+                        fetch(`${urlStock}/stocks/${articleId}`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({size: size, count: count - 1})
+                        }).then(response => response.json()).then(data => {
+                            //console.log(data);
+                        });
+                        listOrder.push(article);
+                    }
+                });
+                fetchPromises.push(response);
+            }
+            await Promise.all(fetchPromises);
+            const order = await client.order.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    totalprice: totalPrice,
+                    status: status,
+                    articles: JSON.stringify(listOrder)
+                }
+            });
+            res.status(200).json(order);
+        }else {
+            res.status(400).json({message: "You can't modify this order. The order is completed or canceled."});
+        }
+    }
+
 });
 
 
